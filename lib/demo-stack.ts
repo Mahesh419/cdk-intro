9456e8813890398c273cd00d71db698f65903590
Mahesh419 <maheshd95@gmail.com>
2021-05-27 11:54:48 +0530

    initial commit

--- lib/demo-stack.ts
+++ lib/demo-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
-
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 export class DemoStack extends cdk.Stack {
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const greetingsLambda = new lambda.Function(this, "greetings-lambda", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: "greetings.handler"
+    });
+
+    new apigateway.LambdaRestApi(this, "greetings-api", {
+      handler: greetingsLambda,
+      deploy: true
+    });
   }
 }
