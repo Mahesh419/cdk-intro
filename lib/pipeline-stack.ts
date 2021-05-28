c2982c9b43b1dbcd9e2863190931cf92a83947df
Mahesh419 <maheshd95@gmail.com>
2021-05-28 12:52:48 +0530

    add cdk pipeline

---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,35 @@
+import * as cdk from '@aws-cdk/core';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import { SimpleSynthAction, CdkPipeline } from "@aws-cdk/pipelines";
+import { SecretValue } from '@aws-cdk/core';
+
+export class DemoPipelineStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const sourceArtifact = new codepipeline.Artifact(); 
+        
+        const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+        new CdkPipeline(this, 'dev-pipeline', {
+            pipelineName: 'cdk-demo-pipeline',
+            cloudAssemblyArtifact,
+
+            sourceAction: new codepipeline_actions.GitHubSourceAction({
+                actionName: 'source', 
+                output: sourceArtifact, 
+                oauthToken: SecretValue.secretsManager('github-token'),
+                owner: 'mahesh419',
+                repo: 'cdk-intro', 
+            }),
+
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact, 
+                cloudAssemblyArtifact, 
+                buildCommand: 'npm run build'
+            })
+        })
+
+    }
+}
